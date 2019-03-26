da03f811a83af36853e41007523a7a47079f6948
Flashcaptain <36227316+Flashcaptain@users.noreply.github.com>
2019-03-26 16:06:30 +0100

    Add bottle capacity

--- VR Bar game/Assets/Scripts/Pooring.cs
+++ VR Bar game/Assets/Scripts/Pooring.cs
@@ -10,11 +10,20 @@ public class Pooring : MonoBehaviour
     [SerializeField]
     private Transform _pooringPosition;
 
+    [SerializeField]
+    private int _capacity;
+
     void Update()
     {
+        if (_capacity == 0)
+        {
+            return;
+        }
+
         if (transform.eulerAngles.x > 80 && transform.eulerAngles.x < 280 || transform.eulerAngles.z > 80 && transform.eulerAngles.z < 280)
         {
             Debug.Log("klunk");
+            _capacity--;
             Instantiate(_water, _pooringPosition.position, transform.rotation);
 
         }
