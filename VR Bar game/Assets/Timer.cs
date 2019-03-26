381033ea4a0faab1cdcd26f60a5e944a0f531c1d
HoekigeHeer <ploxerdongaming@gmail.com>
2019-03-26 14:08:08 +0100

    Timer done

---
+++ VR Bar game/Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] float timer;
+    [SerializeField] float baseTimerValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = baseTimerValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer - Time.deltaTime;
+    }
+
+    public void SetTimer(float value)
+    {
+        timer = value;
+    }
+}
+
+
